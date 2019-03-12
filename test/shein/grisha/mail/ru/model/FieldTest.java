23da8743ed5caf31e9f908d7f47e0f91fef5444b
Grigory Shein <sheingrisha@mail.ru>
2019-03-13 00:42:00 +0300

    fix test due to universal field size

--- test/shein/grisha/mail/ru/model/FieldTest.java
+++ test/shein/grisha/mail/ru/model/FieldTest.java
@@ -12,14 +12,14 @@ public class FieldTest {
 
     @Test
     public void getSize() {
-        final Field field = new Field();
+        final Field field = new Field(3);
 
         assertEquals(3, field.getSize());
     }
 
     @Test
     public void testSetFigure() throws Exception{
-        final Field field = new Field();
+        final Field field = new Field(3);
         final Point inputPoint = new Point(0, 0);
         final Figure inputFigure = Figure.O;
         final Figure expectedFigure = inputFigure;
@@ -45,7 +45,7 @@ public class FieldTest {
 
     @Test
     public void testGetFigureWhenFigureIsNotSet() throws Exception{
-        final Field field = new Field();
+        final Field field = new Field(3);
         final Point inputPoint = new Point(0, 0);
 
         final Figure actualFigure = field.getFigure(inputPoint);
@@ -55,7 +55,7 @@ public class FieldTest {
 
     @Test
     public void testGetFigureWhenXIsLessThenZero() {
-        final Field field = new Field();
+        final Field field = new Field(3);
         final Point inputPoint = new Point(-1, 0);
 
         try {
@@ -66,7 +66,7 @@ public class FieldTest {
 
     @Test
     public void testGetFigureWhenXIsMoreThenSize() {
-        final Field field = new Field();
+        final Field field = new Field(3);
         final Point inputPoint = new Point(3, 0);
 
         try {
@@ -77,7 +77,7 @@ public class FieldTest {
 
     @Test
     public void testGetFigureWhenYIsLessThenZero() {
-        final Field field = new Field();
+        final Field field = new Field(3);
         final Point inputPoint = new Point(0, -1);
 
         try {
@@ -88,7 +88,7 @@ public class FieldTest {
 
     @Test
     public void testGetFigureWhenYIsMoreThenSize() {
-        final Field field = new Field();
+        final Field field = new Field(3);
         final Point inputPoint = new Point(0, 3);
 
         try {
