1eb074f2efcb1d0209fc70d1f978bc826af90c8a
Grigory Shein <sheingrisha@mail.ru>
2019-03-13 00:42:35 +0300

    edit class Field due to desire of universal game

--- src/shein/grisha/mail/ru/model/Field.java
+++ src/shein/grisha/mail/ru/model/Field.java
@@ -1,43 +1,46 @@
 package shein.grisha.mail.ru.model;
 
-import shein.grisha.mail.ru.model.exceptions.AlreadyOccupiedException;
 import shein.grisha.mail.ru.model.exceptions.InvalidPointException;
 
 import java.awt.*;
 
 public class Field {
 
-    public static final int FIELD_SIZE = 3;
-
     public static final int MIN_COORDINATE = 0;
 
-    public static final int MAX_COORDINATE = FIELD_SIZE;
+    private final int fieldSize;
 
-    private final Figure[][] field = new Figure[FIELD_SIZE][FIELD_SIZE];
+    private final Figure[][] field;
+
+
+    public Field(final int fieldSize) {
+        this.fieldSize = fieldSize;
+        field = new Figure[fieldSize][fieldSize];
+    }
 
     public int getSize() {
-        return FIELD_SIZE;
+        return fieldSize;
     }
 
     public Figure getFigure(final Point point) throws InvalidPointException {
-        if(!checkPoint(point)) {
+        if (!checkPoint(point)) {
             throw new InvalidPointException();
         }
         return field[point.x][point.y];
     }
 
     public void setFigure(final Point point, final Figure figure) throws InvalidPointException {
-        if(!checkPoint(point)) {
+        if (!checkPoint(point)) {
             throw new InvalidPointException();
         }
         field[point.x][point.y] = figure;
     }
 
     private boolean checkPoint(final Point point) {
-        return checkCoordinate(point.x) && checkCoordinate(point.y);
+        return checkCoordinate(point.x, field.length) && checkCoordinate(point.y, field[point.x].length);
     }
 
-    private boolean checkCoordinate(final int coordinate) {
-        return coordinate >= MIN_COORDINATE && coordinate < MAX_COORDINATE;
+    private boolean checkCoordinate(final int coordinate, final int maxCoordinate) {
+        return coordinate >= MIN_COORDINATE && coordinate < maxCoordinate;
     }
 }
