e30d581e5017f744549e22a3679876cf7ff2a218
Grigory Shein <sheingrisha@mail.ru>
2019-03-27 01:37:57 +0300

    game improved

--- src/shein/grisha/mail/ru/model/Game.java
+++ src/shein/grisha/mail/ru/model/Game.java
@@ -7,13 +7,25 @@ public class Game {
 
     private final Player [] players;
 
-    private final Field fields;
+    private final Field field;
 
     private final String name;
 
-    public Game(final Player[] players, final Field fields, final String name) {
+    public Game(final Player[] players, final Field field, final String name) {
         this.players = players;
-        this.fields = fields;
+        this.field = field;
         this.name = name;
+    }
+
+    public Player[] getPlayers() {
+        return players;
+    }
+
+    public Field getField() {
+        return field;
+    }
+
+    public String getName() {
+        return name;
     }
 }
